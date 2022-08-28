9507c2207d6562473cf8a1fef857a767e1cb6b8b
ROBERTO ALFREDO MOYA NOACK <ramoyan@correo.url.edu.gt>
2022-08-28 17:18:41 -0600

    Add project files.

---
+++ Lab_01_1273020/AVL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01_1273020
+{
+    internal class AVL
+    {
+    }
+}
---
+++ Lab_01_1273020/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json.Serialization;
+
+namespace Lab_01_1273020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "";
+            while(a=="") //Menú
+            {
+                Console.WriteLine("****Menú****");
+                Console.WriteLine("1- Ingresar un archivo csv");
+                a= Console.ReadLine();
+
+            }
+        }
+    }
+}
