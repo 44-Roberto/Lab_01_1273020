41adc6ab7bf02cdb108d3ae39015b56a593f512a
ROBERTO ALFREDO MOYA NOACK <ramoyan@correo.url.edu.gt>
2022-08-30 23:54:32 -0600

    Insert Funcionando, Delete y Patch agregado

--- Lab_01_1273020/Lista.cs
+++ Lab_01_1273020/Lista.cs
@@ -9,57 +9,67 @@ namespace Lab_01_1273020
 
 
 
-    class Lista<T>
+    class Lista<T> //where T : IComparable<T>
     {
 
-        public class nodo
+        public class nodo : Persona
         {
+
+
+
             public T valor;
             public nodo siguiente;
+
             
+
         }
 
         //declaro mi primer Nodo "Head"
 
         nodo Header = new nodo();
-        
+
 
         public Lista()
-      {
-             Header = null;
-            
-      }
-
-      
-        public void Add_Lista(T Value)
         {
-            nodo nuevoNodo =new nodo();
+            Header = null;
+
+        }
+
+
+        public void Add_Lista(string Name, string Dpi, string BD, string Address, T value)
+        {
+            nodo nuevoNodo = new nodo();
             nodo Temp = Header;
-           
-            nuevoNodo.valor = Value;
+
+            nuevoNodo.valor = value;
+            nuevoNodo.name=Name;
+            nuevoNodo.dpi = Dpi;
+            nuevoNodo.address=Address;
+            nuevoNodo.dateBirth = BD;
 
             if (Header == null)
             {
-                Header=nuevoNodo;
+                Header = nuevoNodo;
+                
                 return;
             }
             else
             {
-                while(Temp.siguiente!= null)
+                while (Temp.siguiente != null)
                 {
-                    Temp= Temp.siguiente;
+                    Temp = Temp.siguiente;
                 }
-                Temp.siguiente=nuevoNodo;
+                Temp.siguiente = nuevoNodo;
                 return;
             }
-          
+
         }
 
-       public T Get(int pos)
+        public T Get(int pos)
         {
             nodo Temp = Header;
             int auxPos = 0;
-            while(Temp!= null)
+            while (Temp.siguiente != null)
             {
                 if (pos == auxPos)
                 {
@@ -71,13 +81,52 @@ namespace Lab_01_1273020
                     auxPos++;
                 }
             }
+           
             return Temp.valor;
         }
 
-        public void delete(int value)
+        public T GetItem(string Name, string Dpi)
         {
-          
+            nodo Temp = Header;
+           
+            while (Temp.siguiente != null)
+            {
+                if ((Temp.name == Name)&& (Temp.dpi == Dpi))
+                {
+                    return Temp.valor;
+                }
+                else
+                {
+                    Temp = Temp.siguiente;
+                    
+                }
+            }
 
+            return Temp.valor;
+        }
+
+        public void delete(string Name, string Dpi)
+        {
+
+            nodo Temp = Header;
+            nodo aux = new nodo();
+            Persona personaaux = new Persona();
+
+            while(Temp!= null)
+            {
+                if( (Temp.name == Name)&&(Temp.dpi == Dpi))
+                {
+                    aux.siguiente = Temp.siguiente;
+                    return;
+                }
+                else
+                {
+                    aux = Temp;
+                    Temp= Temp.siguiente;
+                }
+            }
+            
+            
 
         }
 
--- Lab_01_1273020/Program.cs
+++ Lab_01_1273020/Program.cs
@@ -9,28 +9,111 @@ using System.Text.Json.Serialization;
 
 namespace Lab_01_1273020
 {
-   public class Persona
+   public class Persona 
     {
-        public string Act { get; set; }
+      
         public string name { get; set; }
-        public int dpi { get; set; }
-        public DateTime dateBirth { get; set; }
+        public string dpi { get; set; }
+        public string dateBirth { get; set; }
         public string address { get; set; }
 
     }
     
-    internal class Program
+    internal class Program 
     {
         
         static void Main(string[] args)
         {
+            var reader = new StreamReader(File.OpenRead(@"C:\Users\Roberto Moya\Desktop\Lab1-E2\Lab_01_1273020\Lab_01_1273020\bin\Debug\Prueba.csv"));
+            List<string> list = new List<string>();
+            List<string> list2 = new List<string>();
+            Lista<Persona> listaJSon = new Lista<Persona>();
 
-            //string NombreArchivo = "Prueba.csv";
-           // string jsonString = File.ReadAllText(NombreArchivo);
-            Lista<Persona> listado = new Lista<Persona>();
+            while (!reader.EndOfStream)
+            {
+                var lines = reader.ReadLine();
+                var values = lines.Split(';');
 
+                string jsonString = values[1];
+                Persona personaN = JsonSerializer.Deserialize<Persona>(jsonString);
+
+                if ("INSERT" == values[0])
+                {
+                    listaJSon.Add_Lista(personaN.name, personaN.dpi,personaN.dateBirth,personaN.address, personaN);
+                }
+                else if ("PATCH" == values[0])
+                {
+
+                }
+                else if ("DELETE" == values[0])
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("nada xd");
+                }
+
+
+               // list.Add(values[0]);//Accion
+               // list2.Add(values[1]);//
+
+               
+                
+                //listaJSon.Add_Lista(personaN);
+
+            }
+            
+            
+
+            //Console.WriteLine(listaJSon.GetItem("pablo", "54136137918").name);
+            //Console.WriteLine(listaJSon.Get(1).name);
            
-           
+            for(int i = 0; i < 50; i++)
+            {
+                Console.WriteLine(listaJSon.Get(i).name+"  "+ listaJSon.Get(i).dpi);
+                
+
+            }
+
+            //int indice = 0;
+            /* foreach (var column1 in list)//Tengo los incisos
+             {
+
+                 if ("INSERT" == column1) 
+                 {
+                     listaJSon.Add_Lista()
+                 }
+                 else if ("PATCH"== column1)
+                 {
+
+                 }
+                 else if("DELETE"==column1)
+                 {
+
+                 }
+                 else
+                 {
+                     Console.WriteLine("nada xd");
+                 }
+
+
+                 Console.WriteLine(column1);
+             }
+
+             foreach (var column1 in list2)//Tengo los incisos
+             {
+                 Console.WriteLine(column1);
+             }
+            */
+
+
+
+
+
+
+
+
 
             Console.ReadKey();
         }
